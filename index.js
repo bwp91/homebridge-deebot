5f1b8cf4cdf26988fb0d3167400f50c022f3651f
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-09-15 20:57:04 +0100

    prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 9, -W030, node: true */
 "use strict";
 module.exports = function (homebridge) {
-   let Deebot = require("./lib/deebot.js")(homebridge);
-   homebridge.registerPlatform("homebridge-deebot", "Deebot", Deebot, true);
+  let Deebot = require("./lib/deebot.js")(homebridge);
+  homebridge.registerPlatform("homebridge-deebot", "Deebot", Deebot, true);
 };
