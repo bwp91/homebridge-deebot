57d0082610e8aa30ceaa0496007ee1b422dd0664
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-12-02 11:03:39 +0000

    file/dir structure

--- index.js
+++
@@ -1,5 +0,0 @@
-/* jshint -W014, -W033, esversion: 9 */
-/* eslint-disable new-cap */
-'use strict'
-const deebotPlatform = require('./lib/deebot-platform.js')
-module.exports = hb => hb.registerPlatform('homebridge-deebot', 'Deebot', deebotPlatform)
