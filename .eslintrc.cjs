62a1ccc587fb075fe439ac0907ac2d95809126c7
bwp91 <43026681+bwp91@users.noreply.github.com>
2022-04-15 14:09:41 +0100

    more eslinting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,23 @@
 module.exports = {
   env: {
-    es2021: true,
+    es2022: true,
+    node: true,
   },
   extends: ['airbnb'],
-  plugins: [],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['import', 'sort-exports'],
   rules: {
     camelcase: 'off',
     'import/extensions': ['error',  { js: 'always', json: 'always' }],
+    'import/order': ['warn', { alphabetize: { order: 'asc' } }],
     'max-len': 'off',
     'new-cap': 0,
     quotes: ['error', 'single'],
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 1, maxEOF: 0 }],
     'no-param-reassign': 0,
+    'sort-exports/sort-exports': ['warn', { sortDir: 'asc' }],
   },
 };
